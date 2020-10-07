278402d602ee053c7eb4733245b8569a9aab0e5c
ibrarsoomro <50750853+ibrarsoomro@users.noreply.github.com>
2020-10-08 02:02:07 +0500

    added last name field

--- src/App.js
+++ src/App.js
@@ -5,27 +5,44 @@ const App = () => {
 
     const [name, setName] = useState("");
     const [fullname, setfullName] = useState();
+    const [nametwo, setnameTwo] = useState();
+    const [lastname , setLastName] = useState('');
 
     const InputEvent = (event) => {
         console.log(event.target.value)
         setName(event.target.value);
     }
 
-    const  onsubmit = () => {
+    const onsubmit = (event) => {
+        event.preventDefault();
         setfullName(name)
+        setLastName(nametwo)
+        
+    }
+
+    const InputEventTwo = (event) => {
+        setnameTwo(event.target.value)
+
     }
 
     return (
         <>
-        <div>
-            <h1>Hello {fullname}</h1>
-            <input  type="text"
-             placeholder="Enter your name" 
-             onChange={InputEvent}
-             value={name} />
-            <button onClick={onsubmit}>Click Here</button>
-        </div>
- </>
+            <form onSubmit={onsubmit}>
+                <div>
+                    <h1>Hello {fullname} {lastname}</h1>
+                    <input type="text"
+                        placeholder="Enter your First Name"
+                        onChange={InputEvent}
+                        value={name} />
+
+                    <input type="text"
+                        placeholder="Enter your Last Name"
+                        onChange={InputEventTwo}
+                        value={nametwo} />
+                    <button type="submit">Submit Here</button>
+                </div>
+            </form>
+        </>
     )
 }
 
